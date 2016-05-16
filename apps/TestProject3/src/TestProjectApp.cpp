bb412e8a0a79976c4b1a2c611edda6b13060725a
simongeilfus <simongeilfus@gmail.com>
2016-05-16 18:50:52 +0900

    Added Fog to TestProject3

--- apps/TestProject3/src/TestProjectApp.cpp
+++ apps/TestProject3/src/TestProjectApp.cpp
@@ -146,7 +146,7 @@ void TestProjectApp::update()
 		static float       uFogDensity = 10.0f;
 		static vec3        uSunColor = vec3( 1.0f, 0.77, 0.60 );
 		static vec3        uSunDirection = vec3( 0, 0.3f, -1.0f );
-		static float       uSunDispertion = 3.0f;
+		static float       uSunDispertion = 0.2f;
 		static float       uSunIntensity = 0.16f;
 		static vec3        uInscatteringCoeffs = vec3( 0.0f );
 		
@@ -154,6 +154,8 @@ void TestProjectApp::update()
 		ui::ColorEdit3( "Sun Color", &uSunColor[0] );
 		ui::ColorEdit3( "Inscattering", &uInscatteringCoeffs[0] );
 		ui::DragFloat( "FogDensity", &uFogDensity, 0.1f, 0.0f, 100.0f );
+		ui::DragFloat( "SunDispertion", &uSunDispertion, 0.01f, 0.00001f, 10.0f );
+		ui::DragFloat( "SunIntensity", &uSunIntensity, 0.01f, 0.000001f, 2.0f );
 		
 		mGlslProg->uniform( "uFogColor", uFogColor );
 		mGlslProg->uniform( "uFogDensity", uFogDensity );
