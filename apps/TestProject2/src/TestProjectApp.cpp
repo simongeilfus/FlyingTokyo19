6f90aa8801cf38814aa621c607af0c7d10b5ea9d
simongeilfus <simongeilfus@gmail.com>
2016-05-16 15:54:32 +0900

    updated testproject2

--- apps/TestProject2/src/TestProjectApp.cpp
+++ apps/TestProject2/src/TestProjectApp.cpp
@@ -19,6 +19,58 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+// Base Particle class
+
+// ParticleRef typedef
+typedef std::shared_ptr<class Particle> ParticleRef;
+
+// Particle
+class Particle {
+public:
+	static ParticleRef create( const vec2 &position );
+	Particle( const vec2 &position );
+	
+	void draw();
+	void update();
+	void push( const ci::vec2 &force );
+	
+	vec2 getPosition() const { return mPosition; }
+	vec2 getVelocity() const { return mVelocity; }
+	
+	void setPosition( const vec2 &position ) { mPosition = position; }
+	void setVelocity( const vec2 &velocity ) { mVelocity = velocity; }
+	
+protected:
+	vec2 mPosition;
+	vec2 mVelocity;
+};
+// Particle Implementation
+ParticleRef Particle::create( const vec2 &position )
+{
+	return make_shared<Particle>( position );
+}
+Particle::Particle( const vec2 &position )
+: mPosition( position ), mVelocity( 0.0f )
+{
+}
+void Particle::draw()
+{
+	gl::drawSolidCircle( mPosition, 3.0f );
+}
+
+void Particle::update()
+{
+	mPosition += mVelocity;
+	// turbulence
+	mPosition += Simplex::curlNoise( mPosition * 0.001f ) * 0.5f;
+	mVelocity *= 0.98f;
+}
+
+void Particle::push( const ci::vec2 &force )
+{
+	mVelocity += force;
+}
+
 class TestProjectApp : public App {
 public:
 	void setup() override;
@@ -39,24 +91,48 @@ public:
 	
 	virtual void save( cereal::BinaryOutputArchive &ar );
 	virtual void load( cereal::BinaryInputArchive &ar );
+	
+	vector<ParticleRef> mParticles;
 };
+
+
 
 void TestProjectApp::setup()
 {
 	// initialize ui
 	ui::initialize();
+	
+	// Create particles
+	for( int i = 0; i < 1000; ++i ) {
+		vec2 pos = vec2( randFloat( 0, getWindowWidth() ), randFloat( 0, getWindowHeight() ) );
+		mParticles.push_back( Particle::create( pos ) );
+	}
 }
 
 void TestProjectApp::update()
 {
 	ui::Value( "FPS", (int) getAverageFps() );
+	
+	for( auto particle : mParticles ) {
+		particle->update();
+	}
 }
 void TestProjectApp::draw()
 {
+	gl::clear();
+	
+	for( auto particle : mParticles ) {
+		particle->draw();
+	}
 }
 
 void TestProjectApp::mouseDown( MouseEvent event ) 
 {
+	for( auto particle : mParticles ) {
+		vec2 pos = particle->getPosition();
+		vec2 diff = pos - vec2( event.getPos() );
+		particle->push( -diff * 0.01f );
+	}
 }
 void TestProjectApp::mouseUp( MouseEvent event ) 
 {
